1a7a46a15d1b6c1caa56536b9d6c8a51b12c438c
Dobariya, Chetankumar || mytheresa.com <chetankumar.dobariya@mytheresa.com>
2018-10-20 16:35:04 +0200

    Initial Commit

---
+++ DemoRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoRest
+//
+//  Created by Dobariya, Chetankumar || mytheresa.com on 20.10.18.
+//  Copyright © 2018 Chetan Dobariya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
